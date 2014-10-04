f79bd323193649804cbd55cf9ea73fbc5a6da97b
Michael Meyer <meyer711@gmail.com>
2014-10-04 21:09:42 +0200

    Also capturing error stream

--- src/main/java/com/philmander/jstest/PhantomTestRunner.java
+++ src/main/java/com/philmander/jstest/PhantomTestRunner.java
@@ -29,11 +29,11 @@ public class PhantomTestRunner {
 	private  JsTestLogger logger = null;
 
 	private static final String PHANTOM_QUNIT_RUNNER = "qunit-phantom-runner.js";
-	
+
 	private static final String PASS_PREFIX = "Test passed";
-	
+
 	private static final String FAIL_PREFIX = "Test failed";
-	
+
 	private static final String ERROR_PREFIX = "Test error";
 
 	private String phantom = null;
@@ -42,53 +42,53 @@ public class PhantomTestRunner {
 	 * CLI for phantom test runner
 	 * @param args
 	 */
-	public static void main(String[] args) {		
-		
+	public static void main(String[] args) {
+
 		File currentDir = new File(".");
-		
+
 		CommandLineParser parser = new PosixParser();
 
 		// create the Options
 		Options options = new Options();
 		options.addOption( "p", "phantom", true, "Location of Phantom JS executable" );
-		
-		try {			
-			
+
+		try {
+
 			JsTestLogger logger = new JsTestLogger() {
-				
+
 				public void log(String msg) {
-					System.out.println("[jstest] " + msg);					
+					System.out.println("[jstest] " + msg);
 				}
-				
+
 				public void error(String msg) {
 					System.err.println("[jstest] " + msg);
 				}
 			};
-			
+
 			CommandLine cl = parser.parse(options, args);
-			
+
 			String phantomOption = cl.getOptionValue('p');
 			File phantomLoc = new File(currentDir, phantomOption);
-			
+
 			PhantomTestRunner testRunner = new PhantomTestRunner(phantomLoc.getAbsolutePath());
 			testRunner.setLogger(logger);
-			
+
 			List<String> testFiles = new ArrayList<String>();
 			for(Object arg : cl.getArgList()) {
-				
+
 				File testFile = new File(currentDir, (String)arg);
 				if(testFile.exists()) {
 					testFiles.add(testFile.getAbsolutePath());
 				} else {
-					logger.error("Couldn't find " + testFile.getAbsolutePath()); 
+					logger.error("Couldn't find " + testFile.getAbsolutePath());
 				}
 			}
-			
+
 			logger.log("Using Phantom JS at : " + phantomLoc.getAbsolutePath());
 			logger.log("Running tests on " + testFiles.size() + " files");
-			
+
 			testRunner.runTests(testFiles.toArray(new String[testFiles.size()]));
-									
+
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -98,87 +98,83 @@ public class PhantomTestRunner {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 	}
-	
+
 	/**
 	 * @param phantom The location of the phantom executable
-	 * @throws FileNotFoundException 
+	 * @throws FileNotFoundException
 	 */
 	public PhantomTestRunner(String phantom) throws FileNotFoundException {
-		
+
 		File phantomFile = new File(phantom);
-		if(!phantomFile.exists()) {				
+		if(!phantomFile.exists()) {
 			throw new FileNotFoundException("Phantom executable does not exist for " + SystemUtils.OS_NAME + "(" + phantom + ")");
 		}
-		
+
 		this.phantom = phantom;
 	}
-	
-	public JsTestResults runTests(String[] testFiles) throws IOException {		
-				
+
+	public JsTestResults runTests(String[] testFiles) throws IOException {
+
 		JsTestResults results = new JsTestResults();
-				
+
 		//set up process params
 		String qunitRunner = setupPhantomRunnerFile();
-		
+
 		for (String testFile : testFiles) {
-			
+
 			String loc = fixFilePath(testFile);
 			String[] params = {
 					phantom, qunitRunner, loc
 			};
-			
+
 			//set up process
 			ProcessBuilder processBuilder = new ProcessBuilder(params);
+            processBuilder.redirectErrorStream(true);
 			Process process = processBuilder.start();
-			
+
 			//read and parse output from phantom
-			BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
 			StringBuilder message = new StringBuilder();
-			String line = null;
-            boolean keepProcessing = true;
-            while (keepProcessing) {
-                 line = input.readLine();
-                 if (line != null && line.equalsIgnoreCase("EOF")) {
-                     keepProcessing = false;
-                 } else {
-                     keepProcessing = true;
-                 }
+			String line = reader.readLine();
+            while (line != null && !line.equalsIgnoreCase("EOF")) {
 				//TODO: totally need to refactor reporting parsing
 				if(logger != null) {
 					logger.log(line);
 				}
-							
+
 				message.append(line + "\n");
-				
+
 				if(line.startsWith(PASS_PREFIX) || line.startsWith(FAIL_PREFIX) || line.startsWith(ERROR_PREFIX)) {
-									
+
 					ResultType result;
 					if(line.startsWith(PASS_PREFIX)) {
-						result = ResultType.PASS;						
-					} else if(line.startsWith(FAIL_PREFIX)) {						
-						result = ResultType.FAIL;					
+						result = ResultType.PASS;
+					} else if(line.startsWith(FAIL_PREFIX)) {
+						result = ResultType.FAIL;
 					} else {
 						result = ResultType.ERROR;
 					}
-					
+
 					JsTestResult testResult = new JsTestResult(result, message.toString());
 					results.addResult(testResult);
-					
+
 					//reset message
 					message.setLength(0);
 				}
+
+                line = reader.readLine();
 			}
-			
-			process.destroy();
+
+            destroy(process);
 		}
-				
+
 		return results;
 	}
-	
-	/**
+
+    /**
 	 * Returns a standard summary message on test failure
 	 * @param failCount
 	 * @param errorCount
@@ -201,23 +197,34 @@ public class PhantomTestRunner {
 		String message = "JS tests passed. " + passCount + " test" + pluralPass + " passed.";
 		return message;
 	}
-	
+
 	private String setupPhantomRunnerFile()  throws IOException {
-		
+
 		File tempDir = Files.createTempDir();
-		File tempLoc = new File(tempDir, PHANTOM_QUNIT_RUNNER);		
+		File tempLoc = new File(tempDir, PHANTOM_QUNIT_RUNNER);
 		FileUtils.copyInputStreamToFile(
 				this.getClass().getClassLoader().getResourceAsStream(PHANTOM_QUNIT_RUNNER),
 				tempLoc);
-				
+
 		return tempLoc.getAbsolutePath();
 	}
-	
+
 	private static String fixFilePath(String loc) {
 		File file = new File(loc);
 		String path = "file:///" + file.getAbsolutePath().replace("\\", "/");
 		return path;
 	}
+
+    private void destroy(Process process) {
+        try {
+            // Wait for phantom.exit() to complete
+            process.waitFor();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        }
+
+        process.destroy();
+    }
 
 	/**
 	 * Sets a logger for the test runner to report results to.  Use before calling runTests()
